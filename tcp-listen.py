6116581a33d2bbd2868a132e2feef51b729c9d1e
Jonathan Senkerik <josenk@jintegrate.co>
2018-10-04 08:32:11 -0400

    returns hostname / ip

--- tcp-listen.py
+++ tcp-listen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import socket
 import datetime
@@ -6,18 +6,21 @@ import datetime
 HOST = ''           # '' = Listen on all interfaces.
 PORT = 4001         # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(10)
-    while True:
-        # accept connections from outside
-        conn, addr = s.accept()
-        with conn:
-            t = datetime.datetime.now()
-            strNow = t.strftime('%Y/%m/%d-%H:%M:%S')
-            data = ""
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.close()
-            print('{} Connect {} {} bytes'.format(strNow, addr, len(data)))
+try: 
+    host_name = socket.gethostname() 
+    host_ip = socket.gethostbyname(host_name) 
+except: 
+    print("Unable to get Hostname and IP") 
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+while True:
+    # accept connections from outside
+    conn, addr = s.accept()
+    t = datetime.datetime.now()
+    strNow = t.strftime('%Y/%m/%d-%H:%M:%S')
+    data = conn.recv(1024)
+    conn.send(host_name + " / " + host_ip + "\n")
+    conn.close()
+    print('{} Connect {} {} bytes'.format(strNow, addr, len(data)))
